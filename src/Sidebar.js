e9b46191f13403ddc57f468cf89ed2546c73bac6
Jared <mumawjared@gmail.com>
2022-08-01 09:52:24 -0700

    ai

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -15,9 +15,9 @@ const Sidebar = () => {
 
   const SideBarIcon = ({ icon, text }) => {
     return(
-    <div className="sidebar-icon">
+    <div className="sidebar-icon group">
         {icon}
-        <span className="sidebar-tooltip">
+        <span className="sidebar-tooltip group-hover:scale-100">
             {text}
         </span>
     </div>
