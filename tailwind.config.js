2df894719f6158c28d589ef3301d3b31f18f2ad2
Jared <mumawjared@gmail.com>
2022-08-01 09:47:58 -0700

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "",
+        secondary: "",
+      }
+    },
   },
   plugins: [],
+  variants: {
+
+    extend: {
+      borderRadius: ['hover'],
+
+    },
+
+ },
 }
